0e46b1bd7c22995d8d553796a26f6a492e8fca62
Constantinos Vryonides <constantinos@Constantinoss-MacBook-Pro.local>
2016-04-02 01:55:17 +0100

    Added State serialization/deserialization tests.

--- src/test/java/tool/serializers/vulkan/VulkanStateDeserializerTest.java
+++ src/test/java/tool/serializers/vulkan/VulkanStateDeserializerTest.java
@@ -1,7 +1,22 @@
 package tool.serializers.vulkan;
 
+import org.junit.Before;
+import org.junit.Test;
+import tool.serialization.vulkan.VulkanStateDeserializer;
+
 /**
  * Created by constantinos on 01/04/2016.
  */
 public class VulkanStateDeserializerTest {
+    private VulkanStateDeserializer deserializer;
+
+    @Before
+    public void setup() {
+        deserializer = new VulkanStateDeserializer();
+    }
+
+    @Test
+    public void correctlyDeserializesState() {
+
+    }
 }
--- src/test/java/tool/serializers/vulkan/VulkanStateSerializerTest.java
+++ src/test/java/tool/serializers/vulkan/VulkanStateSerializerTest.java
@@ -1,7 +1,22 @@
 package tool.serializers.vulkan;
 
+import org.junit.Before;
+import org.junit.Test;
+import tool.serialization.vulkan.VulkanStateSerializer;
+
 /**
  * Created by constantinos on 01/04/2016.
  */
 public class VulkanStateSerializerTest {
+    private VulkanStateSerializer serializer;
+
+    @Before
+    public void setup() {
+        serializer = new VulkanStateSerializer();
+    }
+
+    @Test
+    public void correctlySerializesState() {
+
+    }
 }
