9235eb11c02a0bc2a6284a27d55cf4ca29be80b2
Constantinos Vryonides <constantinos@pc-153-114.wireless.hammersmith.generation.lan>
2016-05-18 17:00:30 +0100

    Added basic skeleton for compute stages

--- src/main/java/tool/fsm/vulkan/initializers/ComputeCodeGeneratorsInitializer.java
+++ src/main/java/tool/fsm/vulkan/initializers/ComputeCodeGeneratorsInitializer.java
@@ -1,7 +1,28 @@
 package tool.fsm.vulkan.initializers;
 
+import tool.codegen.coverage.CoverageRandomizer;
 import tool.codegen.vulkan.VulkanCodeGenerator;
+import tool.codegen.vulkan.commandbuffers.VkAllocateCommandBuffersGenerator;
+import tool.codegen.vulkan.commandbuffers.VkBeginCommandBufferGenerator;
+import tool.codegen.vulkan.commandbuffers.VkCreateCommandPoolGenerator;
+import tool.codegen.vulkan.commandbuffers.VkEndCommandBufferGenerator;
+import tool.codegen.vulkan.deallocation.DeallocationGenerator;
+import tool.codegen.vulkan.device.*;
+import tool.codegen.vulkan.enumeration.VkEnumerateInstanceExtensionPropertiesGenerator;
+import tool.codegen.vulkan.enumeration.VkEnumerateInstanceLayerPropertiesGenerator;
+import tool.codegen.vulkan.enumeration.VkEnumeratePhysicalDevicesGenerator;
+import tool.codegen.vulkan.instance.VkApplicationInfoGenerator;
+import tool.codegen.vulkan.instance.VkCreateInstanceGenerator;
+import tool.codegen.vulkan.instance.VkInstanceCreateGenerator;
+import tool.codegen.vulkan.pipelines.VkCreatePipelineCacheGenerator;
+import tool.codegen.vulkan.pipelines.VkCreatePipelineLayoutGenerator;
+import tool.codegen.vulkan.pipelines.VkGetPipelineCacheDataGenerator;
+import tool.codegen.vulkan.resources.*;
+import tool.codegen.vulkan.shaders.VkCreateShaderModuleGenerator;
+import tool.codegen.vulkan.synchronisation.*;
+import tool.codegen.vulkan.utils.RandomSeedGenerator;
 import tool.configs.vulkan.VulkanGlobalState;
+import tool.fsm.vulkan.states.VulkanState;
 
 import java.util.HashMap;
 
@@ -14,7 +35,354 @@ public class ComputeCodeGeneratorsInitializer extends CodeGeneratorsInitializer 
     }
 
     @Override
+    // Initializes the generator hashmap
     public HashMap<String, VulkanCodeGenerator> initializeCodeGenerators() {
-        return null;
+        CoverageRandomizer randomizer = new CoverageRandomizer();
+
+        generators.put(
+                VulkanState.VK_ENUMERATE_INSTANCE_EXTENSION_PROPERTIES.toString(),
+                new VkEnumerateInstanceExtensionPropertiesGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState
+                ));
+
+        generators.put(
+                VulkanState.VK_ENUMERATE_INSTANCE_LAYER_PROPERTIES.toString(),
+                new VkEnumerateInstanceLayerPropertiesGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState
+                ));
+
+        generators.put(
+                VulkanState.VK_APPLICATION_INFO.toString(),
+                new VkApplicationInfoGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_INSTANCE_CREATE_INFO.toString(),
+                new VkInstanceCreateGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_INSTANCE.toString(),
+                new VkCreateInstanceGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_ENUMERATE_PHYSICAL_DEVICES.toString(),
+                new VkEnumeratePhysicalDevicesGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.GET_DEVICE_PROPERTIES.toString(),
+                new DevicePropertiesGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_DEVICE.toString(),
+                new VkCreateDeviceGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_COMMAND_POOL.toString(),
+                new VkCreateCommandPoolGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_ALLOCATE_COMMAND_BUFFERS.toString(),
+                new VkAllocateCommandBuffersGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_DEVICE_QUEUE.toString(),
+                new VkGetDeviceQueueGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_EVENT.toString(),
+                new VkCreateEventGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_SEMAPHORE.toString(),
+                new VkCreateSemaphoreGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_FENCE.toString(),
+                new VkCreateFenceGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_EVENT_STATUS.toString(),
+                new VkGetEventStatusGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_FENCE_STATUS.toString(),
+                new VkGetFenceStatusGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_RESET_EVENT.toString(),
+                new VkResetEventGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_RESET_FENCES.toString(),
+                new VkResetFencesGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_SET_EVENT.toString(),
+                new VkSetEventGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_BUFFER.toString(),
+                new VkCreateBufferGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_BUFFER_VIEW.toString(),
+                new VkCreateBufferViewGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_BUFFER_MEMORY_REQUIREMENTS.toString(),
+                new VkGetBufferMemoryRequirementsGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_IMAGE.toString(),
+                new VkCreateImageGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_IMAGE_VIEW.toString(),
+                new VkCreateImageViewGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_IMAGE_MEMORY_REQUIREMENTS.toString(),
+                new VkGetImageMemoryRequirementsGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_IMAGE_SUBRESOURCE_LAYOUT.toString(),
+                new VkGetImageSubResourceLayoutGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_SHADER_MODULE.toString(),
+                new VkCreateShaderModuleGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_FRAMEBUFFER.toString(),
+                new VkCreateFrameBufferGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_PIPELINE_CACHE.toString(),
+                new VkCreatePipelineCacheGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_CREATE_PIPELINE_LAYOUT.toString(),
+                new VkCreatePipelineLayoutGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_GET_PIPELINE_CACHE_DATA.toString(),
+                new VkGetPipelineCacheDataGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_BEGIN_COMMAND_BUFFER.toString(),
+                new VkBeginCommandBufferGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_END_COMMAND_BUFFER.toString(),
+                new VkEndCommandBufferGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_QUEUE_SUBMIT.toString(),
+                new VkQueueSubmitGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.VK_DEVICE_WAIT_IDLE.toString(),
+                new VkDeviceWaitIdleGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.DEALLOCATION.toString(),
+                new DeallocationGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.RANDOM_SEED.toString(),
+                new RandomSeedGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        generators.put(
+                VulkanState.SELECT_DEVICE.toString(),
+                new SelectDeviceGenerator(
+                        randomStringGenerator,
+                        randomNumberGanerator,
+                        freshMap,
+                        randomizer.randomCoverage(),
+                        globalState));
+
+        return generators;
     }
 }
--- src/main/java/tool/fsm/vulkan/initializers/ComputeStatesInitializer.java
+++ src/main/java/tool/fsm/vulkan/initializers/ComputeStatesInitializer.java
@@ -1,10 +1,13 @@
 package tool.fsm.vulkan.initializers;
 
 import org.statefulj.fsm.model.State;
+import org.statefulj.persistence.memory.MemoryPersisterImpl;
 import tool.fsm.ExitCondition;
 import tool.fsm.vulkan.VulkanEntity;
 import tool.fsm.vulkan.states.VulkanState;
+import tool.fsm.vulkan.transitions.TransitionType;
 
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -21,6 +24,232 @@ public class ComputeStatesInitializer extends StatesInitializer {
 
     @Override
     public void initializeStates() {
+        createStates();
+
+        // Start of sequential  part of the fsm
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.START,
+                VulkanState.RANDOM_SEED);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.RANDOM_SEED,
+                VulkanState.VK_ENUMERATE_INSTANCE_EXTENSION_PROPERTIES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_ENUMERATE_INSTANCE_EXTENSION_PROPERTIES,
+                VulkanState.VK_ENUMERATE_INSTANCE_LAYER_PROPERTIES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_ENUMERATE_INSTANCE_LAYER_PROPERTIES,
+                VulkanState.VK_APPLICATION_INFO);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_APPLICATION_INFO,
+                VulkanState.VK_INSTANCE_CREATE_INFO);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_INSTANCE_CREATE_INFO,
+                VulkanState.VK_CREATE_INSTANCE);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_INSTANCE,
+                VulkanState.VK_CREATE_SURFACE_KHR);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CREATE_SURFACE_KHR,
+                VulkanState.VK_ENUMERATE_PHYSICAL_DEVICES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_ENUMERATE_PHYSICAL_DEVICES,
+                VulkanState.GET_DEVICE_PROPERTIES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.GET_DEVICE_PROPERTIES,
+                VulkanState.VK_CREATE_DEVICE);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_DEVICE,
+                VulkanState.SELECT_DEVICE);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.SELECT_DEVICE,
+                VulkanState.VK_CREATE_COMMAND_POOL);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_COMMAND_POOL,
+                VulkanState.VK_ALLOCATE_COMMAND_BUFFERS);
+
+        // Define random transitions from the VK_ALLOCATE_COMMAND_BUFFERS state
+        VulkanState[] randomStates0 =
+                {
+                        VulkanState.VK_CREATE_SEMAPHORE,
+                        VulkanState.VK_CREATE_EVENT,
+                        VulkanState.VK_CREATE_FENCE,
+                        VulkanState.VK_CREATE_IMAGE,
+                        VulkanState.VK_CREATE_BUFFER,
+                        VulkanState.VK_CREATE_SHADER_MODULE,
+                };
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_ALLOCATE_COMMAND_BUFFERS,
+                VulkanState.VK_CREATE_IMAGE_VIEW);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CREATE_IMAGE_VIEW,
+                VulkanState.VK_CREATE_RENDERPASS);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CREATE_RENDERPASS,
+                VulkanState.VK_CREATE_FRAMEBUFFER);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CREATE_FRAMEBUFFER,
+                randomStates0);
+
+        // End of sequential part
+
+        // Entering random path
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_EVENT,
+                VulkanState.VK_GET_EVENT_STATUS);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_EVENT_STATUS,
+                VulkanState.VK_RESET_EVENT);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_RESET_EVENT,
+                VulkanState.VK_SET_EVENT);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_SET_EVENT,
+                randomStates0);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_FENCE,
+                VulkanState.VK_GET_FENCE_STATUS);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_FENCE_STATUS,
+                VulkanState.VK_RESET_FENCES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_RESET_FENCES,
+                randomStates0);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_SEMAPHORE,
+                randomStates0);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_IMAGE,
+                VulkanState.VK_GET_IMAGE_MEMORY_REQUIREMENTS);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_IMAGE_MEMORY_REQUIREMENTS,
+                VulkanState.VK_GET_IMAGE_SUBRESOURCE_LAYOUT);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_IMAGE_SUBRESOURCE_LAYOUT,
+                randomStates0);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_SHADER_MODULE,
+                VulkanState.VK_CREATE_PIPELINE_CACHE);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_PIPELINE_CACHE,
+                VulkanState.VK_CREATE_PIPELINE_LAYOUT);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_PIPELINE_LAYOUT,
+                VulkanState.VK_CREATE_GRAPHICS_PIPELINES);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_GRAPHICS_PIPELINES,
+                randomStates0);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_BUFFER,
+                VulkanState.VK_CREATE_BUFFER_VIEW);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CREATE_BUFFER_VIEW,
+                VulkanState.VK_GET_BUFFER_MEMORY_REQUIREMENTS);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_BUFFER_MEMORY_REQUIREMENTS,
+                VulkanState.POPULATE_VERTEX_BUFFER);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.POPULATE_VERTEX_BUFFER,
+                VulkanState.VK_ACQUIRE_NEXT_IMAGE_KHR);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_ACQUIRE_NEXT_IMAGE_KHR,
+                VulkanState.VK_BEGIN_COMMAND_BUFFER);
+
+        VulkanState[] nextStates2 =
+                {
+                        VulkanState.VK_CMD_CLEAR_COLOR,
+                        VulkanState.VK_CMD_BEGIN_RENDERPASS
+                };
+
+        defineTransition(TransitionType.SEQUENTIAL_MULTI,
+                VulkanState.VK_BEGIN_COMMAND_BUFFER,
+                nextStates2);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_CMD_CLEAR_COLOR,
+                VulkanState.VK_END_COMMAND_BUFFER);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CMD_BEGIN_RENDERPASS,
+                VulkanState.DRAW_BUFFERS);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.DRAW_BUFFERS,
+                VulkanState.VK_CMD_END_RENDERPASS);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_CMD_END_RENDERPASS,
+                VulkanState.VK_END_COMMAND_BUFFER);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_END_COMMAND_BUFFER,
+                VulkanState.VK_GET_DEVICE_QUEUE);
+
+        defineTransition(TransitionType.REPEATING,
+                VulkanState.VK_GET_DEVICE_QUEUE,
+                VulkanState.VK_QUEUE_SUBMIT);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_QUEUE_SUBMIT,
+                VulkanState.VK_DEVICE_WAIT_IDLE);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.VK_DEVICE_WAIT_IDLE,
+                VulkanState.VK_QUEUE_PRESENT_KHR);
+
+        VulkanState[] randomStates1 =
+                {
+                        VulkanState.VK_ACQUIRE_NEXT_IMAGE_KHR,
+                        VulkanState.STOP,
+                };
+
+        defineTransition(TransitionType.BIASED,
+                VulkanState.VK_QUEUE_PRESENT_KHR,
+                randomStates1);
+
+        defineTransition(TransitionType.SEQUENTIAL,
+                VulkanState.DEALLOCATION,
+                VulkanState.STOP);
+
+        LinkedList<State<VulkanEntity>> fsmStates =
+                new LinkedList<>(states.values());
+
+        persister = new MemoryPersisterImpl<>(fsmStates,
+                states.get(VulkanState.START));
 
     }
 }
